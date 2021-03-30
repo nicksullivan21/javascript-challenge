24babd537938b5aa26498f776880b052b7c5db2e
nicksullivan21 <74960495+nicksullivan21@users.noreply.github.com>
2021-03-30 20:22:21 +0800

    Finished Level 2 app.js, updated index.html

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,6 +5,7 @@ var tableData = data;
 var tBody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // Input data into HTML
@@ -26,17 +27,28 @@ button.on("click", () => {
     var inputDate = inputFieldDate.property("value").trim();
     var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
 
+    // Set up city filter 
+    var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+    var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+
+    // Set up combined filter
+    var combinedFilter = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity)
+
     tBody.html("");
 
     let response = {
-        filterDate
+        filterDate, filterCity, combinedFilter
     }
 
-    // Show data if 1 or more results match search terms
-    if (response.filterDate.length !==0){
-        addData(filterDate);
+    // Show data if 1 or more results match search terms for search using both filters
+    if (response.combinedFilter.length !==0){
+        addData(combinedFilter);
     }
 
+    // Show data if 0 results match search terms for search using both filters but 1 or more result for either date or city filter
+    else if(response.combinedFilter.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
+        addData(filterDate) || addData(filterCity);
+    }
         // Show text if no results
         else {
             tBody.append("tr").append("td").text("No sightings match your search terms.")
